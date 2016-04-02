f6cc235e9d6769d4cfed62554a1d89a3f41e2493
spad <mathieu.galtier@gmx.com>
2016-04-02 19:56:59 +0200

    adapeted settings from previous project

--- tedok/settings.py
+++ tedok/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+# if the environmnent variable WORKING_ENV_TEDOK is not defined
+# then set it to prod
+type_dev = os.environ.get('WORKING_ENV_TEDOK', 'prod')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,7 +27,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gy_31qd&=ehv42oswb#5j934=ejzw)f%j$*y3o9zy+%hia6dnr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if type_dev == 'dev':
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -55,7 +62,7 @@ ROOT_URLCONF = 'tedok.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,13 +81,25 @@ WSGI_APPLICATION = 'tedok.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
 
+if type_dev == 'prod':
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config()
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    # Allow all host headers
+    ALLOWED_HOSTS = ['*']
+else:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('TEDOK_DB_NAME'),
+        'USER': os.environ.get('TEDOK_DB_USER'),
+        'PASSWORD': os.environ.get('TEDOK_DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+    ALLOWED_HOSTS = []
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
@@ -118,4 +137,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+if type_dev == 'prod':
+    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
